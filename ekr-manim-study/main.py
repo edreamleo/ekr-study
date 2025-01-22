ba4ec992b190ed990470671a53a7076a82cc22fe
Edward K. Ream <edreamleo@gmail.com>
2025-01-22 15:29:45 -0600

    Import *live* manimCE files

--- ekr-manim-study/main.py
+++ ekr-manim-study/main.py
@@ -2,6 +2,8 @@
 #@+node:ekr.20250121052813.1: * @file main.py
 #@@language python
 from manim import *
+#@+others
+#@+node:ekr.20250122054014.1: ** DefaultTemplate
 class DefaultTemplate(Scene):
     def construct(self):
         circle = Circle()  # create a circle
@@ -14,4 +16,38 @@ class DefaultTemplate(Scene):
         self.play(Create(square))  # animate the creation of the square
         self.play(Transform(square, circle))  # interpolate the square into the circle
         self.play(FadeOut(square))  # fade out animation
+#@+node:ekr.20250122053953.1: ** SquareAndCircle
+class SquareAndCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+
+        square = Square()  # create a square
+        square.set_fill(BLUE, opacity=0.5)  # set the color and transparency
+
+        square.next_to(circle, RIGHT, buff=0.5)  # set the position
+        self.play(Create(circle), Create(square))  # show the shapes on screen
+#@+node:ekr.20250122115824.1: ** AnimatedSquareToCircle
+class AnimatedSquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        square = Square()  # create a square
+
+        self.play(Create(square))  # show the square on screen
+        self.play(square.animate.rotate(PI / 4))  # rotate the square
+        self.play(Transform(square, circle))  # transform the square into a circle
+        self.play(
+            square.animate.set_fill(PINK, opacity=0.5)
+        )  # color the circle on screen
+#@+node:ekr.20250122120343.1: ** DifferentRotations
+class DifferentRotations(Scene):
+    def construct(self):
+        left_square = Square(color=BLUE, fill_opacity=0.7).shift(2 * LEFT)
+        right_square = Square(color=GREEN, fill_opacity=0.7).shift(2 * RIGHT)
+        self.play(
+            left_square.animate.rotate(PI), Rotate(right_square, angle=PI), run_time=2
+        )
+        self.wait()
+#@-others
+
 #@-leo
